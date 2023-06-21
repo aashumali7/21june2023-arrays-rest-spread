ea1c28da171c405b2112f6ba661100bab024733b
aashumali7 <aashishmali223@gmail.com>
2023-06-21 20:20:40 +0530

    done

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+let x =['aashish'];
+let y =[...x,'MALI']
+
+console.log(y);
+
+let n1=['PM']
+
+let news= [...n1,'modi '];
+let news2= [...news,'yogaday '];
+let news3= [...news2,'event '];
+let news4= [...news3,'at '];
+let news5= [...news4,'UN,HQ '];
+let news6= [...news5,'creates '];
+let news7= [...news6,'guinness '];
+let news8= [...news7,'world '];
+let news9= [...news8,'record '];
+
+console.log(news9);
---
+++ js/custom2.js
@@ -0,0 +1,10 @@
+
+
+//1.function defination
+function getMyFullName(...am){
+    console.log(am);
+
+}
+
+//2. function calling 
+getMyFullName('aashu',"MALI",19,5.8,['a1','a2']);
